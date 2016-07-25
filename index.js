e5fcdb932a4b3ff467f5eb0c39f838237b969b10
Jannis R <mail@jannisr.de>
2016-07-25 22:06:15 +0200

    put everything together

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 'use strict'
 
-// todo
+const createStore = require('redux/lib/createStore').default
+const bind = require('redux/lib/bindActionCreators').bindActionCreators
+const yo = require('yo-yo')
+
+const bindActions = require('./actions')
+const reducer = require('./reducer')
+const ui = require('./ui')
+
+
+
+const store = createStore(reducer)
+const actions = bindActions(store.dispatch)
+
+const render = ui(actions)
+const dom = document.body.appendChild(render(store.getState()))
+
+store.subscribe(() => {
+	const state = store.getState()
+	console.log('state', state)
+	yo.update(dom, render(state))
+})
